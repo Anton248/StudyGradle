1bf996d47f0ec50bfaee3a5bb3692a12f2168841
Anton248 <31551544+Anton248@users.noreply.github.com>
2023-03-18 01:47:01 +0300

    17.03 3

---
+++ src/main/java/ru/nikolski/QA/Task1.java
@@ -0,0 +1,13 @@
+package ru.nikolski.QA;
+import static ru.nikolski.QA.SearchTests.successfulSearchTest;
+import static ru.nikolski.QA.TestingStaticMethod.StaticMethod;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        successfulSearchTest();
+        StaticMethod();
+
+    }
+}
---
+++ src/main/java/ru/nikolski/QA/TestingStaticMethod.java
@@ -0,0 +1,7 @@
+package ru.nikolski.QA;
+
+public class TestingStaticMethod {
+    static void StaticMethod() {
+
+    }
+}
