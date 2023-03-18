f72f9665410a2c0ca0a1d71990ab6b9f7d4c1122
Anton248 <31551544+Anton248@users.noreply.github.com>
2023-03-18 11:59:10 +0300

    Default build.gradle

--- src/main/java/ru/nikolski/QA/Task1.java
+++ src/main/java/ru/nikolski/QA/Task1.java
@@ -1,12 +1,11 @@
 package ru.nikolski.QA;
-import static ru.nikolski.QA.SearchTests.successfulSearchTest;
 import static ru.nikolski.QA.TestingStaticMethod.StaticMethod;
 
 public class Task1 {
 
     public static void main(String[] args) {
 
-        successfulSearchTest();
+        new SearchTests().successfulSearchTest();
         StaticMethod();
 
     }
